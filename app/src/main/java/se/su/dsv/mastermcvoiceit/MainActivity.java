152e111ad5ccdd1a6f7f911d5449be9d12b6c958
Annika Svedin <annika.svedin@gmail.com>
2017-11-24 13:50:15 +0100

    test button

--- app/src/main/java/se/su/dsv/mastermcvoiceit/MainActivity.java
+++ app/src/main/java/se/su/dsv/mastermcvoiceit/MainActivity.java
@@ -1,6 +1,8 @@
 package se.su.dsv.mastermcvoiceit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -8,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void voiceInput(View v) {
+        Toast.makeText(this, "Say something", Toast.LENGTH_SHORT).show();
     }
 }
