a73c02e27a596cfd1ccceb392789f800fdf6b206
Annika Svedin <annika.svedin@gmail.com>
2017-12-05 12:26:15 +0100

    notification in Placeclass

--- app/src/main/java/se/su/dsv/mastermcvoiceit/place/HomePlace.java
+++ app/src/main/java/se/su/dsv/mastermcvoiceit/place/HomePlace.java
@@ -1,21 +1,57 @@
 package se.su.dsv.mastermcvoiceit.place;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
+import android.support.v4.app.NotificationCompat;
+
+import java.util.ArrayList;
 
 import se.su.dsv.mastermcvoiceit.remote.actuator.ActuatorList;
 import se.su.dsv.mastermcvoiceit.remote.sensor.SensorList;
+import se.su.dsv.mastermcvoiceit.R;
 
 /**
  * Created by annika on 2017-12-05.
  */
 
 public class HomePlace extends Place {
+    private NotificationCompat.Builder notificationBuilder;
+    private NotificationManager notificationManager;
 
-    public HomePlace(SensorList sensorList, ActuatorList actuatorList, Location locaion) {
-        super(sensorList, actuatorList, locaion);
+    public HomePlace(Context context, SensorList sensorList, ActuatorList actuatorList, Location locaion) {
+        super(context, sensorList, actuatorList, locaion);
     }
 
     public void tick(Location currentLocation) {
+        if (currentLocation.distanceTo(this.location) < 1000) {
 
+        } else {
+
+        }
+
+    }
+
+    private void createNotification() {
+        notificationBuilder = new NotificationCompat.Builder(this.context)
+//                .setSmallIcon(R.drawable.notification_yellow)
+                .setAutoCancel(true)
+                .setTicker("Ticker")
+                .setWhen(System.currentTimeMillis())
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setPriority(Notification.PRIORITY_MAX);
+    }
+
+    public void sendNotification(
+            String heading,
+            String message,
+            ArrayList<Notification.Action> notificatinActions,
+            Intent intent,
+            PendingIntent pendingIntent
+    )  {
+        // set notification and send
     }
 }
--- app/src/main/java/se/su/dsv/mastermcvoiceit/place/Place.java
+++ app/src/main/java/se/su/dsv/mastermcvoiceit/place/Place.java
@@ -1,7 +1,16 @@
 package se.su.dsv.mastermcvoiceit.place;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
+import android.support.v4.app.NotificationCompat;
 
+import java.util.ArrayList;
+
+import se.su.dsv.mastermcvoiceit.MainActivity;
 import se.su.dsv.mastermcvoiceit.remote.actuator.ActuatorList;
 import se.su.dsv.mastermcvoiceit.remote.sensor.SensorList;
 
@@ -10,14 +19,18 @@ import se.su.dsv.mastermcvoiceit.remote.sensor.SensorList;
  */
 
 public abstract class Place {
-    private SensorList sensorList;
-    private ActuatorList actuatorList;
-    private Location locaion;
+    protected Context context;
+    protected SensorList sensorList;
+    protected ActuatorList actuatorList;
+    protected Location location;
 
-    public Place(SensorList sensorList, ActuatorList actuatorList, Location locaion) {
+
+
+
+    public Place(Context context, SensorList sensorList, ActuatorList actuatorList, Location location) {
         this.sensorList = sensorList;
         this.actuatorList = actuatorList;
-        this.locaion = locaion;
+        this.location = location;
     }
 
     public abstract void tick(Location currentLocation);
