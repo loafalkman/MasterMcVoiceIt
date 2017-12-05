1aecc3a1065aea6713b90efedfd8484c4e01ed98
Annika Svedin <annika.svedin@gmail.com>
2017-12-05 16:01:02 +0100

    Place

--- app/src/main/java/se/su/dsv/mastermcvoiceit/place/HomePlace.java
+++ app/src/main/java/se/su/dsv/mastermcvoiceit/place/HomePlace.java
@@ -1,27 +1,17 @@
 package se.su.dsv.mastermcvoiceit.place;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.location.Location;
-import android.support.v4.app.NotificationCompat;
-
-import java.util.ArrayList;
 
 import se.su.dsv.mastermcvoiceit.remote.actuator.ActuatorList;
 import se.su.dsv.mastermcvoiceit.remote.sensor.SensorList;
-import se.su.dsv.mastermcvoiceit.R;
 
-import static android.content.Context.NOTIFICATION_SERVICE;
 
 /**
  * Created by annika on 2017-12-05.
  */
 
 public class HomePlace extends Place {
-    private NotificationManager notificationManager;
 
     public HomePlace(Context context, SensorList sensorList, ActuatorList actuatorList, Location locaion) {
         super(context, sensorList, actuatorList, locaion);
@@ -33,61 +23,8 @@ public class HomePlace extends Place {
     }
 
     public void tick(Location currentLocation) {
-        if (currentLocation.distanceTo(this.location) < 1000) {
-            // test!
-
-            testNotification("Close!", "Question");
-
-        } else {
-
+        if (currentLocation.distanceTo(super.location) < 1000) {
+            // someting!
         }
-
     }
-
-    private void testNotification(String title, String message) {
-        NotificationCompat.Builder builder = createNotification();
-        builder.setContentTitle(title);
-        builder.setContentText(message);
-
-        Intent resultIntent = createIntent();
-        PendingIntent pendingIntent = createPendingIntent(resultIntent);
-        builder.setContentIntent(pendingIntent);
-
-        notificationManager = (NotificationManager) this.context.getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(9999, builder.build());
-
-
-    }
-
-    private NotificationCompat.Builder createNotification() {
-        NotificationCompat.Builder nb = new NotificationCompat.Builder(this.context)
-//                .setSmallIcon(R.drawable.notification_yellow)
-                .setAutoCancel(true)
-                .setTicker("Ticker")
-                .setWhen(System.currentTimeMillis())
-                .setDefaults(Notification.DEFAULT_ALL)
-                .setPriority(Notification.PRIORITY_MAX);
-
-        return nb;
-    }
-
-    private Intent createIntent() {
-        Intent resultIntent = new Intent(this.context, HomePlace.class);
-        resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        return resultIntent;
-    }
-
-    private PendingIntent createPendingIntent(Intent resultIntent) {
-                return PendingIntent.getActivity(this.context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-    }
-
-//    public void sendNotification(
-//            String heading,
-//            String message,
-//            ArrayList<Notification.Action> notificatinActions,
-//            Intent intent,
-//            PendingIntent pendingIntent
-//    )  {
-//        // set notification and send
-//    }
 }
--- app/src/main/java/se/su/dsv/mastermcvoiceit/place/Place.java
+++ app/src/main/java/se/su/dsv/mastermcvoiceit/place/Place.java
@@ -1,16 +1,8 @@
 package se.su.dsv.mastermcvoiceit.place;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.location.Location;
-import android.support.v4.app.NotificationCompat;
 
-import java.util.ArrayList;
-
-import se.su.dsv.mastermcvoiceit.MainActivity;
 import se.su.dsv.mastermcvoiceit.remote.actuator.ActuatorList;
 import se.su.dsv.mastermcvoiceit.remote.sensor.SensorList;
 
@@ -23,9 +15,6 @@ public abstract class Place {
     protected SensorList sensorList;
     protected ActuatorList actuatorList;
     protected Location location;
-
-
-
 
     public Place(Context context, SensorList sensorList, ActuatorList actuatorList, Location location) {
         this.sensorList = sensorList;
